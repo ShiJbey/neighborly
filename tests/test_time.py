e29a60fd2dc5fa847d0f8731baa88d5bbcb51e0e
Shi Johnson-Bey <shijbey@gmail.com>
2023-03-09 07:57:02 -0800

    refactored core.time module

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,3 +1,5 @@
+import pytest
+
 from neighborly.core.time import SimDateTime, TimeDelta
 
 
@@ -33,17 +35,17 @@ def test__sub__():
 def test__add__():
     date = SimDateTime()
     new_date = date + TimeDelta(months=5, days=27)
-    assert new_date.month == 5
-    assert new_date.day == 27
-    assert date.month == 0
-    assert date.day == 0
+    assert new_date.month == 6
+    assert new_date.day == 28
+    assert date.month == 1
+    assert date.day == 1
 
 
 def test__iadd__():
     date = SimDateTime()
     date += TimeDelta(months=5, days=27)
-    assert date.month == 5
-    assert date.day == 27
+    assert date.month == 6
+    assert date.day == 28
 
 
 def test__le__():
@@ -75,14 +77,16 @@ def test__eq__():
     assert (SimDateTime() == SimDateTime(year=2000)) is False
     assert (SimDateTime(year=3000) == SimDateTime()) is False
     assert (SimDateTime(year=3000) == SimDateTime(year=3000)) is True
+    assert SimDateTime(1, 4, 6) == SimDateTime(1, 4, 6)
+    assert SimDateTime(2023, 6, 5) == SimDateTime(2023, 6, 5, 0)
 
 
 def test_to_date_str():
     date = SimDateTime(2022, 6, 27)
-    assert date.to_date_str() == "Sat, 27/06/2022 @ 00:00"
+    assert date.to_date_str() == "Fri, 27/06/2022 @ 00:00"
 
     date = SimDateTime(2022, 9, 3)
-    assert date.to_date_str() == "Wed, 03/09/2022 @ 00:00"
+    assert date.to_date_str() == "Tue, 03/09/2022 @ 00:00"
 
 
 def test_to_iso_str():
@@ -95,69 +99,75 @@ def test_to_iso_str():
 
 def test_to_hours():
     date = SimDateTime(2022, 6, 27)
-    assert date.to_hours() == 16310088
+    assert date.to_hours() == 16_301_328
 
     date = SimDateTime(2022, 9, 3)
-    assert date.to_hours() == 16311528
-
-
-def test_to_ordinal():
-    date = SimDateTime(2022, 6, 27)
-    assert date.to_ordinal() == 679587
-
-    date = SimDateTime(2022, 9, 3)
-    assert date.to_ordinal() == 679647
-
-
-def test_from_ordinal():
-    date = SimDateTime.from_ordinal(679710)
-    assert date.day == 10
-    assert date.hour == 0
-    assert date.weekday == 3
-    assert date.month == 11
-    assert date.year == 2022
+    assert date.to_hours() == 16_302_768
 
 
 def test_from_iso_str():
-    date = SimDateTime.from_iso_str("2022-11-10T00:36:19.362Z")
-    assert date.day == 10
-    assert date.hour == 0
-    assert date.weekday == 3
-    assert date.month == 11
-    assert date.year == 2022
+    date = SimDateTime.from_str("2022-11-10T00:36:19.362Z")
+    assert date == SimDateTime(2022, 11, 10)
 
 
 def test_from_str():
-    date = SimDateTime.from_str("2-10-3-1")
-    assert date.day == 3
-    assert date.hour == 1
-    assert date.weekday == 3
-    assert date.month == 10
-    assert date.year == 2
+    date = SimDateTime.from_str("03/10/0002")
+    assert date == SimDateTime(2, 10, 3)
 
 
 def test_increment():
-    date = SimDateTime()
+    date = SimDateTime(1, 1, 1)
     date.increment(hours=26)
-    assert date.hour == 2
-    assert date.day == 1
+    assert date == SimDateTime(1, 1, 2, 2)
     date.increment(hours=24, days=4)
-    assert date.hour == 2
-    assert date.day == 6
+    assert date == SimDateTime(1, 1, 7, 2)
     date.increment(days=28)
-    assert date.hour == 2
-    assert date.day == 6
-    assert date.month == 1
+    assert date == SimDateTime(1, 2, 7, 2)
     date.increment(months=12)
-    assert date.month == 1
-    assert date.year == 1
+    assert date == SimDateTime(2, 2, 7, 2)
+
+    date = SimDateTime(2022, 12, 27)
+    date.increment(days=1)
+    assert date == SimDateTime(2022, 12, 28)
+    date.increment(days=1)
+    assert date == SimDateTime(2023, 1, 1)
+    date.increment(days=336)
+    assert date == SimDateTime(2024, 1, 1)
 
 
 def test__init__():
-    time = SimDateTime()
-    assert time.day == 0
-    assert time.hour == 0
-    assert time.weekday == 0
-    assert time.month == 0
-    assert time.year == 0
-    assert time.weekday_str == "Sunday"
+    d = SimDateTime()
+    assert d.day == 1
+    assert d.hour == 0
+    assert d.weekday == 0
+    assert d.month == 1
+    assert d.year == 1
+    assert d.weekday_str == "Sunday"
+
+    d = SimDateTime(2001, 7, 13, 8)
+    assert d.day == 13
+    assert d.hour == 8
+    assert d.weekday == 5
+    assert d.month == 7
+    assert d.year == 2001
+    assert d.weekday_str == "Friday"
+
+    with pytest.raises(ValueError):
+        # Year cannot be less than 1
+        d = SimDateTime(-1, 10, 10)
+
+    with pytest.raises(ValueError):
+        # Month cannot be less than 1
+        d = SimDateTime(0, 10, 10)
+
+    with pytest.raises(ValueError):
+        # Month cannot be greater than 12
+        d = SimDateTime(2023, 13, 10)
+
+    with pytest.raises(ValueError):
+        # Day cannot be less than 1
+        d = SimDateTime(2023, 12, 0)
+
+    with pytest.raises(ValueError):
+        # Day cannot be greater than 28
+        d = SimDateTime(2023, 13, 29)
