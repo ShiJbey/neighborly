d7d713ba29ebc4b5a1c97e632bf5f5ce44b76d35
Shi Johnson-Bey <shijbey@gmail.com>
2023-04-24 16:00:30 -0700

    version update

--- src/neighborly/__init__.py
+++ src/neighborly/__init__.py
@@ -13,6 +13,7 @@ from neighborly.core.ecs import (
 )
 from neighborly.core.time import SimDateTime, TimeDelta
 from neighborly.simulation import Neighborly, PluginInfo
+from neighborly.__version__ import VERSION
 
 __all__ = [
     "NeighborlyConfig",
@@ -28,4 +29,5 @@ __all__ = [
     "TimeDelta",
     "IComponentFactory",
     "Event",
+    "VERSION",
 ]
